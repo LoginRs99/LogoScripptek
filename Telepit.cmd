91b6abfa7d230524570c4e958640525c11b4e58e
LoginRs99 <kissbence199@gmail.com>
2024-08-09 16:58:12 +0200

    Create Telepit.cmd

---
+++ Telepit.cmd
@@ -0,0 +1,91 @@
+@echo off
+:: Ellenorzi, hogy a script rendszergazdai jogosultsagokkal fut-e
+:: openfiles parancsot hasznalunk, mert ha nem rendszergazdai, nem fog mukodni
+openfiles >nul 2>&1
+if '%errorlevel%' NEQ '0' (
+    :: A script nem fut rendszergazdai jogosultsagokkal
+    :: Rendszergazdai jogosultsagokkal ujrainditjuk a scriptet
+    echo Az ujrainditas folyamatban...
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit /b
+)
+
+:MENU
+cls
+echo Valassz egy opciot:
+echo 1. Programok/winget telepitese
+echo 2. Szamitogep informacioi legyujtese
+echo 3. Office Aktivator
+echo 4. Windows Aktivator
+echo 5. Kilepes
+set /p option=Valasztas:
+
+if "%option%"=="1" goto DownloadAndRun
+if "%option%"=="2" goto DownloadToDesktop
+if "%option%"=="3" goto OfficeActivator
+if "%option%"=="4" goto WindowsActivator
+if "%option%"=="5" exit
+goto MENU
+
+:DownloadAndRun
+:: Letoltes es futtatas
+echo Letoltes folyamatban...
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/LoginRs99/LogoScripptek/main/telepites.ps1' -OutFile '%TEMP%\telepites.ps1'"
+
+:: Ellenorzi, hogy a letoltes sikeres volt-e
+if exist "%TEMP%\telepites.ps1" (
+    echo A script letoltese sikeres volt.
+    echo A script futtatasa...
+    powershell -Command "Start-Process powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File %TEMP%\telepites.ps1' -Verb RunAs"
+) else (
+    echo Hiba tortent a script letoltese soran.
+)
+pause
+goto MENU
+
+:DownloadToDesktop
+:: Letoltes az asztalra
+echo Letoltes folyamatban...
+set "desktop=%USERPROFILE%\Desktop"
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/LoginRs99/LogoScripptek/main/Informaciok.bat' -OutFile '%desktop%\Informaciok.bat'"
+
+:: Ellenorzi, hogy a letoltes sikeres volt-e
+if exist "%desktop%\Informaciok.bat" (
+    echo A batch file letoltese sikeres volt az asztalra.
+) else (
+    echo Hiba tortent a batch file letoltese soran.
+)
+pause
+goto MENU
+
+:OfficeActivator
+:: Letoltes es futtatas
+echo Letoltes folyamatban...
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/LoginRs99/LogoScripptek/main/Officeactivator.cmd' -OutFile '%TEMP%\Officeactivator.cmd'"
+
+:: Ellenorzi, hogy a letoltes sikeres volt-e
+if exist "%TEMP%\Officeactivator.cmd" (
+    echo A script letoltese sikeres volt.
+    echo A script futtatasa...
+    call "%TEMP%\Officeactivator.cmd"
+) else (
+    echo Hiba tortent a script letoltese soran.
+)
+pause
+goto MENU
+
+:WindowsActivator
+:: Letoltes es futtatas
+echo Letoltes folyamatban...
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/LoginRs99/LogoScripptek/main/winactivator.cmd' -OutFile '%TEMP%\winactivator.cmd'"
+
+:: Ellenorzi, hogy a letoltes sikeres volt-e
+if exist "%TEMP%\winactivator.cmd" (
+    echo A script letoltese sikeres volt.
+    echo A script futtatasa...
+    call "%TEMP%\winactivator.cmd"
+) else (
+    echo Hiba tortent a script letoltese soran.
+)
+pause
+goto MENU
